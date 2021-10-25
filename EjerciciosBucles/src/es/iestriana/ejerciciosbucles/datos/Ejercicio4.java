5c2cf709fef92bdf38346b1037f7ec3ec3b44e0a
usuario <usuario@usuario-H310M-S2H-2-0>
2021-10-25 14:32:41 +0200

    Bucles

--- EjerciciosBucles/src/es/iestriana/ejerciciosbucles/datos/Ejercicio4.java
+++ EjerciciosBucles/src/es/iestriana/ejerciciosbucles/datos/Ejercicio4.java
@@ -1,14 +1,34 @@
 package es.iestriana.ejerciciosbucles.datos;
 
+import java.util.Scanner;
+
 public class Ejercicio4 {
 
 	/*
 	 * Display the product of the first n odd numbers
 	 */
 	
+	private static Scanner sc = new Scanner(System.in);
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int numero = 0;
+		do {
+			System.out.println("Nº: ");
+			numero = sc.nextInt();
+		} while (numero <= 0);
+		
+		int i = 1;
+		long productoFinal = 1;
+		while (numero > 0) {
+			productoFinal = productoFinal * i;
+			i = i + 2;
+			
+			numero--;
+		}
+		
+		System.out.println("Producto: " + productoFinal);
+		
+		sc.close();
 	}
 
 }
