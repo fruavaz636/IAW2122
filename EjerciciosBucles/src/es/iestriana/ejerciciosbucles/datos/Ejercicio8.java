137c97dc46f0e5888b36fd55e5d67975c91704a0
usuario <usuario@usuario-H310M-S2H-2-0>
2021-10-27 14:35:39 +0200

    Ejercicios

--- EjerciciosBucles/src/es/iestriana/ejerciciosbucles/datos/Ejercicio8.java
+++ EjerciciosBucles/src/es/iestriana/ejerciciosbucles/datos/Ejercicio8.java
@@ -1,5 +1,7 @@
 package es.iestriana.ejerciciosbucles.datos;
 
+import java.util.Scanner;
+
 public class Ejercicio8 {
 
 	/*
@@ -7,9 +9,39 @@ public class Ejercicio8 {
 	 * there are between 1 and the number
 	 */
 	
+	private static Scanner sc = new Scanner(System.in);
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int numero;
+		do {
+			System.out.println("Nº: ");
+			numero = sc.nextInt();
+		} while (numero <= 0);
+		
+		int contadorPrimos = 0;
+		
+		for (int i = 1; i <= numero; i++) {
+			if (esPrimo(i)) {
+				contadorPrimos++;
+			}
+		}
+		
+		System.out.println("Entre 1 y " + numero + " hay " + 
+						contadorPrimos + " números primos");
 
+		sc.close();
+	}
+
+	private static boolean esPrimo(int i) {
+		boolean primo = true;
+		
+		for (int j = 2; j < i; j++) {
+			if (i%j==0) {
+				primo = false;
+			}
+		}
+		
+		return primo;
 	}
 
 }
