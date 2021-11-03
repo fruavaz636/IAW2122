717346ef9b889e73ec001d622aca2296ce41282d
usuario <usuario@usuario-H310M-S2H-2-0>
2021-11-03 14:29:06 +0100

    Ejercicios

--- EjerciciosTablas/src/es/ejerciciostablas/datos/Ejercicio2.java
+++ EjerciciosTablas/src/es/ejerciciostablas/datos/Ejercicio2.java
@@ -2,6 +2,12 @@ package es.ejerciciostablas.datos;
 
 public class Ejercicio2 {
 
+	/*
+	 * Read the arrays of n numbers and 
+	 * save the even number in another array 
+	 * and the odd numbers in another array
+	 */
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
