34936e78ce05f4f521e075e31110d903c8234af3
usuario <usuario@usuario-H310M-S2H-2-0>
2022-01-26 14:13:59 +0100

    lista libros

--- webLibros/src/main/java/es/iestriana/dao/LibroDAO.java
+++ webLibros/src/main/java/es/iestriana/dao/LibroDAO.java
@@ -1,5 +1,12 @@
 package es.iestriana.dao;
 
+import java.util.List;
+
+import es.iestriana.bean.Conexion;
+import es.iestriana.bean.Libro;
+import es.iestriana.bean.Usuario;
+
 public interface LibroDAO {
 
+	public List<Libro> listarLibros(Conexion c, Usuario u);
 }
--- webLibros/src/main/java/es/iestriana/dao/LibroDAOBD.java
+++ webLibros/src/main/java/es/iestriana/dao/LibroDAOBD.java
@@ -1,5 +1,46 @@
 package es.iestriana.dao;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import es.iestriana.bean.Conexion;
+import es.iestriana.bean.Libro;
+import es.iestriana.bean.Usuario;
+
 public class LibroDAOBD implements LibroDAO {
+
+	@Override
+	public List<Libro> listarLibros(Conexion c, Usuario u) {
+		List<Libro> libros = new ArrayList<Libro>();		
+		
+		String query = "SELECT * FROM libros WHERE idUsuario = ?";
+		
+		try {
+			PreparedStatement ps = c.getConector().prepareStatement(query);
+			ps.setInt(1, u.getIdUsuario());
+			
+			ResultSet rs = ps.executeQuery();
+			
+			while (rs.next()) {
+				Libro auxiliar = new Libro(rs.getInt("idLibro"), 
+						rs.getString("titulo"), 
+						rs.getString("autor"), 
+						rs.getString("isbn"), 
+						rs.getBytes("portada"), 
+						rs.getInt("idUsuario"), 
+						rs.getString("uuid"));
+				
+				libros.add(auxiliar);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return libros;
+	}
 
 }
